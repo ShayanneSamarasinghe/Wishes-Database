fcb3ecaff69cfeea04aa2bda9b41d52279b37319
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-06-25 12:06:03 -0400

    Fixed issue with database

--- schema.sql
+++ schema.sql
@@ -1,13 +1,12 @@
 
-
 Create Database wishes_Db;
-Use wishes_Db
+Use wishes_Db;
 
 -- Inside of that database, make a table called "wishes" which will have a wish column and an id column. 
 -- The id will be automatically incremented while also being the primary key.
 
-create table wishes(
+Create table wishes(
     id integer auto_increment primary key,
     wish varchar (255)
     
-)
+);
