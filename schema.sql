f717bb64256b793930e667ea9bd76096935a9a84
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-06-25 11:51:07 -0400

    Added columns to databse file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,13 @@
 
 
 Create Database wishes_Db;
+Use wishes_Db
+
+-- Inside of that database, make a table called "wishes" which will have a wish column and an id column. 
+-- The id will be automatically incremented while also being the primary key.
+
+create table wishes(
+    id integer auto_increment primary key,
+    wish varchar (255)
+    
+)
