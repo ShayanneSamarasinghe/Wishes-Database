b6805453ce5a38166c8ad941398156c9eaa1bfea
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-06-25 13:15:54 -0400

    Added post route and get route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mysql = require("mysql")
 const express = require ("express")
 const exphbs  = require('express-handlebars')
 
+const app = express();
+
+const port = 3000
+
 class Database {
     constructor( config ) {
         this.connection = mysql.createConnection( config );
@@ -30,9 +34,27 @@ class Database {
   
 const db = new Database({
     host: "localhost",
-    port: 3306,
+    port: port,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
     insecureAuth : true
 });
+
+// GET ROUTE //
+app.get("/", function (req, res){
+    res.send("I am a get request")
+    res.render('wishes', function (err, html) {
+        // ...
+      })
+})
+
+// POST ROUTE //
+app.post('/', function (req, res) {
+    res.send('I am a post request')
+})
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
